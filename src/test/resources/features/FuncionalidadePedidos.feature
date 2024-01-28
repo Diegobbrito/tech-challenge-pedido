4a6eb6324d2cb36342112c916e14d9efc9b63318
Diego Brito <diego.brito@f1rst.com.br>
2024-01-28 20:27:55 -0300

    atualização do testes de bdd

---
+++ src/test/resources/features/FuncionalidadePedidos.feature
@@ -0,0 +1,17 @@
+# language: pt
+Funcionalidade: API - Pedidos
+
+  @smoke
+  Cenário: Criar um pedido
+    Quando submeter um novo pedido
+    Então o pedido é gerado com sucesso
+
+  Cenário: Consultar todos os pedidos
+    Dado que um pedido já foi registrado
+    Quando requisitar a busca de todos os pedidos
+    Então os pedidos são exibidos com sucesso
+
+  Cenário: Atualizar statis do pedido
+    Dado que um pedido já foi registrado
+    Quando requisitar a atualização de um pedido
+    Então o pedido é atualizado
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,19 +1,28 @@
 CREATE DATABASE IF NOT EXISTS lanchonete;
 USE lanchonete;
 
-CREATE TABLE produtos (
+CREATE TABLE status_pedido (
     id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(200) NOT NULL,
-    descricao VARCHAR(200) NOT NULL,
-    valor DECIMAL(10,2) NOT NULL,
-    imagem_url VARCHAR(200),
-    categoria_id INT NOT NULL,
+    titulo VARCHAR(200) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE categorias (
+CREATE TABLE pedidos (
     id INT NOT NULL AUTO_INCREMENT,
-    titulo VARCHAR(200) NOT NULL,
-    descricao VARCHAR(200),
+    data_criacao DATE,
+    valor DECIMAL(10,2) NOT NULL,
+    is_cliente BOOLEAN NOT NULL,
+    cliente_id INT,
+    status_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE pedido_produto (
+    id INT NOT NULL AUTO_INCREMENT,
+    pedido_id INT NOT NULL,
+    produto_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id),
     PRIMARY KEY (id)
 );
