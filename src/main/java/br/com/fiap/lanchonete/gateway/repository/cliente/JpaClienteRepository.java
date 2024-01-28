d4735c231ffead609ab3dbedca8981e68d0a8825
Diego Brito <diego.brito@f1rst.com.br>
2024-01-28 15:49:44 -0300

    remoção de classes não usadas

--- src/main/java/br/com/fiap/lanchonete/gateway/repository/cliente/JpaClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.lanchonete.gateway.repository.cliente;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface JpaClienteRepository extends JpaRepository<ClienteEntity, Integer> {
-    Optional<ClienteEntity> findByCpf(String cpf);
-}
--- src/main/java/br/com/fiap/lanchonete/gateway/repository/pedido/JpaProdutoPedidoRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.fiap.lanchonete.gateway.repository.pedido;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaProdutoPedidoRepository extends JpaRepository<ProdutoPedidoEntity, Integer> {
-}
