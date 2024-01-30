3141ba0ab393d163e96e5a2bbd8203e9a478c1dc
Diego Brito <dibbrito@gmail.com>
2024-01-30 18:35:35 -0300

    atualizacao de adapter

--- src/main/java/br/com/fiap/pedido/api/adapter/ProdutoAdapter.java
+++ src/main/java/br/com/fiap/pedido/api/adapter/ProdutoAdapter.java
@@ -2,17 +2,10 @@ package br.com.fiap.pedido.api.adapter;
 
 import br.com.fiap.pedido.core.entity.Produto;
 import br.com.fiap.pedido.gateway.dataprovider.produto.ProdutoDtoResponse;
-import br.com.fiap.pedido.gateway.dataprovider.produto.ProdutoEntity;
 
 import java.math.BigDecimal;
 
 public class ProdutoAdapter {
-
-    public static Produto toProduto(ProdutoEntity produtoEntity) {
-        final var produto = new Produto(produtoEntity.getNome(), produtoEntity.getDescricao(), produtoEntity.getValor(), produtoEntity.getImagemUrl());
-        produto.setId(produtoEntity.getId());
-        return produto;
-    }
 
     public static Produto toProduto(ProdutoDtoResponse dto) {
         final var produto = new Produto(
