c975a96ae8c014b17014f1c1ad5811b0b751f9f1
Diego Brito <dibbrito@gmail.com>
2024-03-03 17:43:58 -0300

    desenvolvimento inicial da fila

--- src/main/java/br/com/fiap/pedido/gateway/messaging/IPagamentoQueue.java
+++ src/main/java/br/com/fiap/pedido/gateway/messaging/IPagamentoQueue.java
@@ -2,11 +2,10 @@ package br.com.fiap.pedido.gateway.messaging;
 
 import br.com.fiap.pedido.core.entity.Pedido;
 import br.com.fiap.pedido.core.entity.Produto;
-import br.com.fiap.pedido.gateway.dataprovider.pagamento.PagamentoDtoResponse;
 
 import java.util.List;
 
 public interface IPagamentoQueue {
 
-    PagamentoDtoResponse criarPagamento(Pedido entity, List<Produto> produtos);
+    void criarPagamento(Pedido entity, List<Produto> produtos);
 }
--- src/main/java/br/com/fiap/pedido/gateway/messaging/pagamento/PagamentoQueue.java
+++ src/main/java/br/com/fiap/pedido/gateway/messaging/pagamento/PagamentoQueue.java
@@ -3,34 +3,48 @@ package br.com.fiap.pedido.gateway.messaging.pagamento;
 import br.com.fiap.pedido.api.adapter.PedidoAdapter;
 import br.com.fiap.pedido.core.entity.Pedido;
 import br.com.fiap.pedido.core.entity.Produto;
-import br.com.fiap.pedido.gateway.dataprovider.IPagamentoDataProvider;
 import br.com.fiap.pedido.gateway.dataprovider.pagamento.CriarPagamentoDto;
-import br.com.fiap.pedido.gateway.dataprovider.pagamento.PagamentoDtoResponse;
 import br.com.fiap.pedido.gateway.messaging.IPagamentoQueue;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.google.gson.Gson;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
 
 import java.util.HashMap;
 import java.util.List;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import java.util.Map;
 
 @Component
 public class PagamentoQueue implements IPagamentoQueue {
 
-    @Autowired
-    public PagamentoQueue(){
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${queue.pagamentos.pendentes}")
+    private String filaPagamentosPendentes;
+
+    public PagamentoQueue(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @RabbitListener(queues = {"${queue.pagamento}"})
     @Override
-    public PagamentoDtoResponse criarPagamento(Pedido entity, List<Produto> produtos) {
-        return null;
+    public void criarPagamento(Pedido entity, List<Produto> produtosList) {
+        List<CriarPagamentoDto.ProdutoDto> produtos = PedidoAdapter.toRequest(entity.getProdutos(), produtosList);
+        CriarPagamentoDto dto = new CriarPagamentoDto(
+                produtos,
+                entity.getCliente(),
+                entity.getValor(),
+                entity.getId());
+
+        var message = toMessage(dto);
+
+        rabbitTemplate.convertAndSend(filaPagamentosPendentes, message);
+    }
+
+    public static String toMessage(CriarPagamentoDto pagamento){
+        Map<String, Object> message = new HashMap<>();
+        message.put("cpf",pagamento.cpf());
+        return new Gson().toJson(message);
     }
 }
