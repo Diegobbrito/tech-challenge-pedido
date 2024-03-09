6200d8c0ca189c75ea84ce14f2309bca5b973556
Diego Brito <dibbrito@gmail.com>
2024-03-09 08:53:37 -0300

    add schema to run local

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO status_pedido (titulo) VALUES ('Pagamento Pendente');
+INSERT INTO status_pedido (titulo) VALUES ('Recebido');
+INSERT INTO status_pedido (titulo) VALUES ('Em preparação');
+INSERT INTO status_pedido (titulo) VALUES ('Pronto');
+INSERT INTO status_pedido (titulo) VALUES ('Finalizado');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE status_pedido (
+    id INT NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE pedidos (
+    id INT NOT NULL AUTO_INCREMENT,
+    data_criacao DATE,
+    valor DECIMAL(10,2) NOT NULL,
+    cliente VARCHAR(20),
+    status_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE pedido_produto (
+    id INT NOT NULL AUTO_INCREMENT,
+    pedido_id INT NOT NULL,
+    produto_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+    PRIMARY KEY (id)
+);
